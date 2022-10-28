aa4f05f7dc0cc318ec821ffe71698c8810536ea8
zubair2001 <zubair_mansuri2001@outlook.com>
2022-10-28 20:53:31 +0500

    completed

--- src/components/Api.js
+++ src/components/Api.js
@@ -8,6 +8,10 @@ export const geoApiOptions = {
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY="237fb7f987a10cfb44293bd3c23e309f";
+
 fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions', geoApiOptions)
 	.then(response => response.json())
 	.then(response => console.log(response))
